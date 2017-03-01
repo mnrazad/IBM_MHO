494b8ff3c15c641912f0f68f9526cd23813f5466
Azad <mnrazad@gmail.com>
2017-02-28 21:47:38 -0500

    adding moonlight service page

--- moonLight/ServicePageObjects.java
+++ moonLight/ServicePageObjects.java
@@ -1,5 +1,41 @@
 package moonLight;
 
-public class ServicePageObjects {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class ServicePageObjects {
+	
+	
+	WebDriver tdriver;
+	 
+	  // Constructor to setup the Webdriver
+	public ServicePageObjects(WebDriver driver){
+		 
+		
+		tdriver= driver; 
+	
+		 
+	}
+	 
+	 
+	 
+	  // Functions to get objects location for login page
+	 
+	 public WebElement getVeritus(){
+		 
+		  WebElement veritusNet= tdriver.findElement(By.xpath(".//*[@id='ibm-primary-links']/li[3]/ul/li[1]/a"));
+		 return veritusNet ; 
+		 
+	 }
+	  
+	  public WebElement getservice(){
+			 
+		  WebElement serviceObject = tdriver.findElement(By.xpath(".//*[@id='ibm-primary-links']/li[3]/ul/li[1]/a"));
+			 return serviceObject ; 
+			 
+		 }
+	  
 }
+	  
+	  
